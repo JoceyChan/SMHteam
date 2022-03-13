121a856665644df9ad4130c44bbd6d27131d5fb3
Roman <aroman27@ucmerced.edu>
2022-03-12 21:58:34 -0800

    Created backend server, and working API endpoint for text to speech

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SMHteam
+
+To run backend:
+1. install node.js 
+2. cd into screen-exchange-backend folder
+3. run "npm install"
+4. create a ".env" file
+5. in the ".env" file add the following
+    PORT = 8080
+    GOOGLE_APPLICATION_CREDENTIALS = ""
+6. since the google text to speech requires an API key to authenticate, contact @Alex to 
+    receive a key. Alternatively you may create your own google cloud project with text to speech
+    and use the api key from that
+7. you should be good to run the backend server. just run "npm start" 
---
+++ Screen-Exchange-Backend/src/server.js
@@ -0,0 +1,65 @@
+import 'dotenv/config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import textToSpeech from '@google-cloud/text-to-speech';
+import fs from 'fs';
+import util from 'util';
+
+
+
+//create google text to speech client
+const client = new textToSpeech.TextToSpeechClient();
+
+//start express server
+const app = express();
+
+//setup parser for json body requests
+app.use(bodyParser.json());
+
+
+/**
+ *  The function return a base64 encoded audio file
+ * @param {string} text 
+ * @returns {bytes} audio
+ */
+async function getAudioByteStream(text) {
+    // Construct the request
+    const request = {
+        input: {text: text},
+        // Select the language and SSML Voice
+        voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+
+        // Select the type of audio encoding
+        audioConfig: {audioEncoding: 'MP3'},
+    };
+
+    // Performs the Text-to-Speech request
+    return await client.synthesizeSpeech(request);
+}
+
+
+/**
+ * The function return a base64 encoded audio file to client
+ * @param {string} text
+ * @return {json} audio
+ */
+app.post("/text-to-speech", (req, res) => {
+    const text = req.body.text;
+    console.log(text);
+
+    getAudioByteStream(text).then(audio => {
+        res.json({ audio });
+        res.status(200);
+        res.send();
+    }).catch(err => {
+        console.error('ERROR:', err);
+        res.status(500);
+        res.send();
+    });
+    
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT);
+});
+
