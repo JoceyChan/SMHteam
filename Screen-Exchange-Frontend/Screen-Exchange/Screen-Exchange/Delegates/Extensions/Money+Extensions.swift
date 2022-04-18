32e5be5452cfb32acceb78ab36015840f04b1552
alroman2 <40187237+alroman2@users.noreply.github.com>
2022-04-18 00:01:15 -0700

    Added swift money package pod

---
+++ Screen-Exchange-Frontend/Screen-Exchange/Screen-Exchange/Delegates/Extensions/Money+Extensions.swift
@@ -0,0 +1,26 @@
+////
+////  Amount.swift
+////  Screen-Exchange
+////
+////  Created by Alex Roman on 4/17/22.
+////
+//
+//import Foundation
+//import Money
+//
+//protocol Amount {
+//    var total: Decimal { get }
+//    var currency: CurrencyType.Type { get }
+//}
+//
+//extension Money: Amount {
+//    var total: Decimal {
+//        <#code#>
+//    }
+//}
+//
+//struct Prod {
+//    var amount: Amount
+//}
+//
+//let prod = Prod(amount: 12.00 as Money<USD>)
